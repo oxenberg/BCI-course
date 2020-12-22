2d185496a34e7e4d396825deae20d556d08913ef
Or Oxenberg <oxenberg14@gmail.com>
2020-12-22 10:58:54 +0200

    init commit

---
+++ HW_1/collectData.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 20 20:24:18 2020
+
+@author: oxenb
+"""
+
+from pyOpenBCI import OpenBCICyton
+import numpy as np
+import mne
+import random
+import time
+
+CH_AMOUNT = 16
+TIME_BETWEEN_EVENTS = 3
+SAMPLE_RATE = 125
+TIME_BETWEEN_EVENTS_RATE = SAMPLE_RATE*TIME_BETWEEN_EVENTS
+
+uVolts_per_count = (4500000)/24/(2**23-1) #uV/count
+
+EXP_NAME = "Or_1_raw.fif"
+
+
+EXPERIMENT_DURATION = 600
+ITER = {"COUNT" : 0} #for cout the time 
+ACTIONS = {1 : "LEFT",2 : "RIGHT",3 : "NONE"}
+
+RUN_EXP = False
+if RUN_EXP:
+    board = OpenBCICyton(port='COM3', daisy=True)
+start_time = time.time()
+current_time = start_time
+#########################
+
+array_data = []
+stim  = []
+
+
+#: create the raw object from array
+
+def create_raw_data(results,stim):
+    ch_names = ['EEG ' + str(ID) for ID in range(CH_AMOUNT)]
+    ch_type = 'eeg'
+    info = mne.create_info(ch_names,SAMPLE_RATE,ch_type)
+    rawData = mne.io.RawArray(results,info)
+    
+    #: add events data to raw
+    stim_info = mne.create_info(['STI'], rawData.info['sfreq'], ['stim'])
+    stim = np.expand_dims(stim, axis=0) 
+    stim_raw = mne.io.RawArray(stim, stim_info)
+    rawData.add_channels([stim_raw], force_update_info=True)
+    
+    
+    
+    #eventsData = mne.find_events(rawData, stim_channel='STI')
+    return rawData
+
+
+def run_expirement(sample):
+    data = np.array(sample.channels_data)* uVolts_per_count
+    
+    all_time = time.time() - start_time
+    ITER["COUNT"] +=1
+    if ITER["COUNT"]% TIME_BETWEEN_EVENTS_RATE == 0 :
+         int_action = random.randint(1, 3)
+         print(ACTIONS[int_action])
+         stim.append(int_action)
+    else:
+        stim.append(0)
+    array_data.append(data)
+    
+    # print((all_time,event_time) )
+    
+    if int(all_time) >= EXPERIMENT_DURATION:
+        board.stop_stream()
+    
+def start_expirement():
+    
+    board.start_stream(run_expirement)
+    board.disconnect()
+
+    
+if RUN_EXP:
+    start_expirement()
+    
+    
+    ##data exploration
+    #: transform to array format
+    array_data = np.array(array_data)
+    array_data = array_data.transpose()
+    array_data_v = array_data* 10**(-6) #: to volt
+    
+    #: transform to raw (mne) format
+    rawData = create_raw_data(array_data_v,stim)
+    
+    #: filter electrecy 50 hz freq
+    rawData = rawData.filter(None, 45., fir_design='firwin')
+    
+    #: mainualy filtering
+    events = mne.find_events(rawData, stim_channel='STI')
+    
+    
+    annot_from_events = mne.annotations_from_events(
+        events=events, event_desc=ACTIONS, sfreq=rawData.info['sfreq'])
+    rawData.set_annotations(annot_from_events)
+    
+    rawData.plot()
+    
+    #: save data after cleaning
+    rawData.save(EXP_NAME,overwrite=True)
+
+else:
+    rawData = mne.io.read_raw_fif(EXP_NAME, preload=True)
+    
+    events = mne.find_events(rawData, stim_channel='STI')
+
+    event_dict = {'LEFT': 1, 'RIGHT': 2, 'NONE': 3}
+    
+    
+    rawData.plot_psd(fmax=50)
+    
+    
+    fig = mne.viz.plot_events(events,event_id  = event_dict, sfreq=rawData.info['sfreq'],
+                              first_samp=rawData.first_samp)
+    
+    reject_criteria = dict(eeg=150e-6)       # 250 µV
+    
+    
+    epochs = mne.Epochs(rawData, events, event_id=event_dict, tmin=-0.1, tmax=0.2,preload=True)
+    
+    left_epochs = epochs['LEFT']
+    right_epochs = epochs['RIGHT']
+    none_epochs = epochs['NONE']
+    
+    
+    left_epochs = left_epochs.average()
+    right_epochs = right_epochs.average()
+    none_epochs = none_epochs.average()
+    
+    mne.viz.plot_compare_evokeds(dict(left=left_epochs, right=right_epochs,nothing = none_epochs),
+                                  legend='upper left', show_sensors='upper right')
+    
+    
+    epochs.plot_image(combine='mean')
+
+
+#########################
+
+
+
+
+
+
+
+    
+    
+    
+    
+
---
+++ HW_1/createModel.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 21 21:38:29 2020
+
+@author: oxenb
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from mne_features.feature_extraction import FeatureExtractor
+
+from sklearn.pipeline import Pipeline
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import ShuffleSplit, cross_val_score
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import (GridSearchCV, cross_val_score,
+                                     StratifiedKFold)
+
+from mne import Epochs, pick_types, events_from_annotations
+import mne
+from mne.decoding import (SlidingEstimator, GeneralizingEstimator, Scaler,
+                          cross_val_multiscore, LinearModel, get_coef,
+                          Vectorizer, CSP)
+
+
+EXP_NAME = "Or_1_raw.fif"
+
+features = ['app_entropy', 'decorr_time', 'higuchi_fd',
+            'hjorth_complexity', 'hjorth_complexity_spect', 'hjorth_mobility',
+            'hjorth_mobility_spect', 'hurst_exp', 'katz_fd', 'kurtosis',
+            'line_length', 'mean', 'ptp_amp', 'samp_entropy',
+            'skewness', 'spect_edge_freq', 'spect_entropy', 'spect_slope',
+            'std', 'svd_entropy', 'svd_fisher_info', 'teager_kaiser_energy',
+            'variance', 'wavelet_coef_energy', 'zero_crossings', 'max_cross_corr',
+            'nonlin_interdep', 'phase_lock_val', 'spect_corr', 'time_corr']
+
+def preprocess():
+
+    tmin, tmax = -1., 4.
+    
+    raw = mne.io.read_raw_fif(EXP_NAME, preload=True)
+    
+    raw.filter(7., 40., fir_design='firwin', skip_by_annotation='edge')
+    
+    events = mne.find_events(raw, 'STI')
+    
+    picks = pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False,
+                       exclude='bads')
+    
+    
+    event_id = {'Left': 1, 'right': 2,'none': 3}
+    
+    epochs = Epochs(raw, events, event_id, tmin, tmax, proj=True, picks=picks,
+                    baseline=None, preload=True)
+    epochs.pick_types(eeg=True, exclude='bads')  # remove stim and EOG
+
+    return epochs,raw
+
+def trainCSP_LDA(epochs_data_train,labels,cv):
+    # Assemble a classifier
+    lda = LinearDiscriminantAnalysis()
+    csp = CSP(n_components=14, reg=None, log=True, norm_trace=False)
+    
+    # Use scikit-learn Pipeline with cross_val_score function
+    clf = Pipeline([('CSP', csp), ('LDA', lda)])
+    scores = cross_val_score(clf, epochs_data_train, labels, cv=cv, n_jobs=1)
+    
+    # Printing the results
+    class_balance = np.mean(labels == labels[0])
+    class_balance = max(class_balance, 1. - class_balance)
+    print("Classification accuracy: %f / Chance level: %f" % (np.mean(scores),
+                                                              class_balance))
+
+def train_mne_feature(data,labels,raw):
+    pipe = Pipeline([('fe', FeatureExtractor(sfreq=raw.info['sfreq'],
+                                         selected_funcs=["std","app_entropy",
+                                                         "mean",
+                                                         "kurtosis",
+                                                         "max_cross_corr"])),
+                 ('scaler', StandardScaler()),
+                 ('clf', GradientBoostingClassifier())])
+    skf = StratifiedKFold(n_splits=5, random_state=42)
+    y = labels
+    
+    params_grid = {'fe__app_entropy__emb': np.arange(2, 5)}
+
+    gs = GridSearchCV(estimator=pipe, param_grid=params_grid,
+                      cv=StratifiedKFold(n_splits=5, random_state=42), n_jobs=1,
+                      return_train_score=True)
+    gs.fit(data, y)
+    
+    
+    scores = pd.DataFrame(gs.cv_results_)
+    print(scores[['params', 'mean_test_score', 'mean_train_score']])
+    # Best parameters obtained with GridSearchCV:
+    print(gs.best_params_)
+    
+    gs_best = gs.best_estimator_
+    new_scores = cross_val_score(gs_best, data, y, cv=skf)
+
+    print('Cross-validation accuracy score (with optimized parameters) = %1.3f '
+          '(+/- %1.5f)' % (np.mean(new_scores), np.std(new_scores)))
+    
+    return pipe
+def main():
+    epochs,raw =  preprocess()
+    
+    labels = epochs.events[:, -1]
+
+    # get MEG and EEG data
+    epochs_data_train = epochs.get_data()
+    
+    cv = ShuffleSplit(10, test_size=0.2, random_state=42)
+    
+    # trainCSP_LDA(epochs_data_train,labels,cv)
+    
+    pipe = train_mne_feature(epochs_data_train,labels,raw)
+    
+    return pipe
+
+if __name__ == '__main__':
+    pipe = main()
+
+
+
+'''
+['app_entropy', 'decorr_time', 'energy_freq_bands', 'higuchi_fd',
+ 'hjorth_complexity', 'hjorth_complexity_spect', 'hjorth_mobility'
+ 'hjorth_mobility_spect', 'hurst_exp', 'katz_fd', 'kurtosis', 'line_length',
+ 'mean', 'pow_freq_bands', 'ptp_amp', 'samp_entropy', 'skewness', 
+ 'spect_edge_freq', 'spect_entropy', 'spect_slope', 'std', 'svd_entropy',
+ 'svd_fisher_info', 'teager_kaiser_energy', 'variance', 'wavelet_coef_energy',
+ 'zero_crossings', 'max_cross_corr', 'nonlin_interdep', 'phase_lock_val',
+ 'spect_corr', 'time_corr']
+'''
+
